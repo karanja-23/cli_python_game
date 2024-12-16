67d1b36cea0775c4d8b71a3d8bd059945126e2e2
Hosea <karanja.mungai23@gmail.com>
2024-12-16 14:07:15 +0300

    added game component, authentication and a sqlite database

---
+++ lib/helpers.py
@@ -0,0 +1,12 @@
+from sqlalchemy import  String, Integer, Column, Table, MetaData
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class User (Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    wins = Column(Integer)
+    losses = Column(Integer)
+    password = Column(String)
